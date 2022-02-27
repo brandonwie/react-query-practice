0070b68da9feccb55858b95d80e457f29b477fef
Brandon Wie <brandonwie.cs@gmail.com>
2022-02-28 00:43:05 +0900

    create traditional API using useEffect and React Query to compare

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, '/**/*.scss')],
   },
   reactStrictMode: true,
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import '../styles/globals.css';
 import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <nav className={styles.nav}>
         <Link href={'/'}>Home</Link>
         <Link href={'/super-heroes'}>Traditional Super Heroes</Link>
         <Link href={'/rq-super-heroes'}>RQ Super Heroes</Link>
       </nav>
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
 
