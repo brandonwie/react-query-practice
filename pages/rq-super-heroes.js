58b0ec3b68d7ca11f2cea2f892dfc24df3c6b3aa
Brandon Wie <brandonwie.cs@gmail.com>
2022-03-12 23:06:31 +0900

    onSuccess, onError, select - modify fetched data

--- pages/rq-super-heroes.js
+++ pages/rq-super-heroes.js
@@ -5,12 +5,24 @@ const fetchSuperHeroes = async () => {
   return await axios.get('http://localhost:4000/superheroes');
 };
 export default function RQSuperHeroes() {
+  const onSuccess = (data) => {
+    console.log('Perform side effect after data fetching', data);
+  };
+  const onError = (err) => {
+    console.log('Perform side effect after error', err);
+  };
   // at least two arguments, unique key, a function that returns a promise
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
-      enabled: false,
+      onSuccess,
+      onError, // run after 3 times error happens
+      // This option can be used to transform or select a part of the data returned by the query function.
+      select: (data) => {
+        const superHeroNames = data.data.map((hero) => hero.name);
+        return superHeroNames;
+      },
     }
   );
 
@@ -28,8 +40,8 @@ export default function RQSuperHeroes() {
     <div>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={refetch}>Fetch heroes</button>
-      {data?.data.map((hero) => {
-        return <div key={hero.name}>{hero.name}</div>;
+      {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>;
       })}
     </div>
   );
